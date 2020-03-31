37c2aca6e59b4253b8e4be4210374d9465091996
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-03-31 17:19:18 +0200

    Use non persisted data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const port = process.env.PORT || 8080;
 const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const data = require("../db.json");
 
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
 server.use(middlewares);
 
+const router = jsonServer.router(data);
 server.use(router);
+
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
